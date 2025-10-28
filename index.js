4285181cb538490b6f85f9a7e9851213fe97a7a5
HARIHARAN K <140387416+hariharan1476@users.noreply.github.com>
2025-10-28 18:42:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('<h1>Hello from CI/CD demo!for DEVOPS by Hariharan k hiiiiii</h1><p>Deployed at: ' + new Date().toISOString() + '</p>');
+app.use(express.static("public"));
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
 });
-
-app.listen(port, () => console.log(`Listening on ${port}`));
